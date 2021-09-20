71fb7fbca83beaf6eeb73739cc70869f2a12822b
tony <tony-94@bk.ru>
2021-09-20 13:36:24 +0300

    Added tests for local, real, selenoid and browserstack devices

---
+++ src/test/java/tests/real/RealTestBase.java
@@ -0,0 +1,42 @@
+package tests.real;
+
+import com.codeborne.selenide.Configuration;
+import drivers.RealMobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.Attach.getSessionId;
+
+public class RealTestBase {
+    @BeforeAll
+    public static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = RealMobileDriver.class.getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+    }
+
+    @BeforeEach
+    public void startDriver() {
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = getSessionId();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+
+        closeWebDriver();
+
+    }
+}
